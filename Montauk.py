d190cccb0676b7582541d94acf395fc0c6047cad
Matthew Gentry <matthew.gentry91@gmail.com>
2018-12-19 17:41:23 -0500

    Cleaned up the code.

--- Montauk.py
+++ Montauk.py
@@ -2,12 +2,27 @@ import tweepy
 import time
 from credentials import *
 from weather import *
-import random
+
+hashtags = ['#weather', '#LIWeather', '#NYWeather', '#LongIsland', 'Montauk', '#EastLI', '#EasternLI']
+
+
+def hashtag_append():
+    hashtags.append(hashtag1)
+    hashtags.append(hashtag2)
+
+
+def hashtag_pop():
+    hashtags.pop(0)
+    hashtags.pop(0)
+
+
+def sleep():
+    time.sleep(3600)
+
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-hashtags = ['#weather', '#LIWeather', '#NYWeather', '#LongIsland', 'Montauk', '#EastLI', '#EasternLI']
 
 weather = Weather(unit=Unit.FAHRENHEIT)
 location = weather.lookup_by_location('11572')
@@ -21,9 +36,7 @@ while True:
     tweet = 'It is ' + str(condition.text).lower() + ' and ' + str(conditionTemp) + chr(176) +  \
             ' in Montauk, NY. ' + hashtag1 + ' ' + hashtag2 + ' '
     api.update_status(status=tweet)
-    hashtags.append(hashtag1)
-    hashtags.append(hashtag2)
-    hashtags.pop(0)
-    hashtags.pop(0)
-    time.sleep(3600)
+    hashtag_append()
+    hashtag_pop()
+    sleep()
     continue
