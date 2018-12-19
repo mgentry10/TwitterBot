9a94c6b5b1f037d361c776d777e53c733bf279e7
Matthew Gentry <matthew.gentry91@gmail.com>
2018-12-18 21:13:15 -0500

    Deleted time stuff, fixed spacing in tweets.

--- Commack.py
+++ Commack.py
@@ -1,6 +1,5 @@
 import tweepy
 import time
-import datetime
 from credentials import *
 from weather import *
 import random
@@ -8,8 +7,6 @@ import random
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-ts = time.time()
-st = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
 hashtags = ['#weather', '#LIWeather', '#NYWeather', '#LongIsland', 'Commack', '#Suffolk']
 
 weather = Weather(unit=Unit.FAHRENHEIT)
@@ -19,7 +16,7 @@ condition = location.condition
 
 while True:
     tweet = 'It is ' + str(condition.text).lower() + ' and ' + str(conditionTemp) + chr(176) +  \
-            ' in Commack, NY.' + random.choice(hashtags) + ' ' + random.choice(hashtags) + ' ' + random.choice(hashtags)
+            ' in Commack, NY. ' + random.choice(hashtags) + ' ' + random.choice(hashtags) + ' ' + random.choice(hashtags)
     api.update_status(status=tweet)
     time.sleep(3600)
     continue
--- Oceanside.py
+++ Oceanside.py
@@ -1,6 +1,5 @@
 import tweepy
 import time
-import datetime
 from credentials import *
 from weather import *
 import random
@@ -8,8 +7,6 @@ import random
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-ts = time.time()
-st = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
 hashtags = ['#weather', '#LIWeather', '#NYWeather', '#LongIsland', '#Oceanside', '#Nassau']
 
 weather = Weather(unit=Unit.FAHRENHEIT)
@@ -18,8 +15,12 @@ conditionTemp = location.condition.temp
 condition = location.condition
 
 while True:
+    hashtag1 = random.choice(hashtags)
+    hashtag2 = random.choice(hashtags)
+    hashtag3 = random.choice(hashtags)
+
     tweet = 'It is ' + str(condition.text).lower() + ' and ' + str(conditionTemp) + chr(176) +  \
-            ' in Oceanside, NY.' + random.choice(hashtags) + ' ' + random.choice(hashtags) + ' ' + random.choice(hashtags)
+            ' in Oceanside, NY. ' + hashtag1 + ' ' + hashtag2 + ' ' + hashtag3
     api.update_status(status=tweet)
     time.sleep(3600)
     continue
